05c3fd95015cd793fb9e7210b6ba235e0e1db643
Adam Skoczylas <skocadam@gmail.com>
2018-03-06 21:20:55 +0100

    add event from client to server

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const app = require('express')();
 const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
 	res.sendfile('index.html');
 });
 
+//Whenever someone connects this gets executed
+io.on('connection', (socket) => {
+	console.log('A user connected');
+
+	setTimeout(function () {
+		socket.emit('superDuperEvent', { text: 'A custom event named superDuperEvent!' });
+	}, 4000);
+	
+	socket.on('superDuperClientEvent', (data) => {
+		console.log(data);
+	});
+
+	socket.on('disconnect', () => {
+		console.log('A user disconnected');
+	});
+});
+
 http.listen(3000, () => {
-	console.log('listening on port *:3000);
+	console.log('listening on port *:3000');
 });
